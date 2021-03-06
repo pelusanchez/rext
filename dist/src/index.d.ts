b6aaeeba3537ff7d2e52a24b63fc972a5bb74d3b
David <davidiglesanchez@gmail.com>
2021-03-06 18:33:04 +0100

    Removed src from dist

--- dist/src/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { RextEditor } from './editor';
--- dist/src/lib/color.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Color space functions
- */
-import { vec3 } from "../models/models";
-export declare const getLuma: (rgb_pix: vec3) => number;
-export declare const hsv2rgb: (pixel_hsv: vec3) => vec3;
-export declare const asArray: (vec: vec3) => number[];
