290706e55c39354b3c7744be587788ad264b7aff
pelusanchez <davidiglesanchez@gmail.com>
2021-10-11 12:53:45 +0200

    Added test image.

--- src/models/models.ts
+++ src/models/models.ts
@@ -46,7 +46,7 @@ export interface Params extends ParamsAbs {
 	darkFill: number;
 	darkColor: number;
 	darkSat: number;
-  rotation: number;
-  scale: f2Number;
-  translate: f2Number;
+    rotation: number;
+    scale: f2Number;
+    translate: f2Number;
 }
